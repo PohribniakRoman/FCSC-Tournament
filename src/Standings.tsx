1bc38c9f7e64884d4e2b3a724a4fb86dc02e0208
PohribniakRoman <79878288+PohribniakRoman@users.noreply.github.com>
2023-07-21 17:50:14 +0300

    add adaptive

--- src/Standings.tsx
+++ src/Standings.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import {BsFillGrid3X2GapFill} from "react-icons/bs";
+
 const StandingsData = [
   { name: "asd1", scroe: { wins: 1, loses: 8 } },
   { name: "asd2", scroe: { wins: 2, loses: 7 } },
@@ -48,12 +51,19 @@ const ScheduleData = [
 ];
 
 export const Standings: React.FC = () => {
+  const [isVisible,setVisible] = useState(false);
   const currentStandingsData = StandingsData.sort(
     (a, b) => b.scroe.wins - a.scroe.wins
   );
 
   return (
-    <section className="standings">
+    <>
+      <div className="standings--toggle">
+        <div className="standings--toggle-btn" onClick={()=>setVisible(!isVisible)}>
+          <BsFillGrid3X2GapFill/>
+      </div>
+    </div>
+    <section className={`standings ${isVisible?"active":""}`}>
       <div className="standings--score-container">
         <div className="standings--item">
           <h1>
@@ -65,7 +75,7 @@ export const Standings: React.FC = () => {
         </div>
         {currentStandingsData.map((e) => {
           return (
-            <div className="standings--item">
+            <div key={e.name} className="standings--item">
               <p className="standings--item-title">
                 <i>{e.name}</i>
               </p>
@@ -88,9 +98,9 @@ export const Standings: React.FC = () => {
             <h1>Team 2</h1>
           </div>
         </div>
-        {ScheduleData.map((e) => {
+        {ScheduleData.map((e,i) => {
           return (
-            <div className="standings--schedule-item">
+            <div key={i} className="standings--schedule-item">
               <div className="standings--schedule-container">
                 <h1>{e.participants[0].name}</h1>
               </div>
@@ -124,5 +134,6 @@ export const Standings: React.FC = () => {
         })}
       </div>
     </section>
+    </>
   );
 };
